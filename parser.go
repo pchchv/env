860db42619fc822c970a60e321c455af269e5c21
pchchv <ipchchv@gmail.com>
2023-04-05 11:12:54 +0300

    Implement an isSpace and isLineEnd funcs

--- parser.go
+++ parser.go
@@ -40,3 +40,20 @@ func getStatementStart(src []byte) []byte {
 
 	return getStatementStart(src[pos:])
 }
+
+// isSpace tells whether the rune is a space character but not a line feed character,
+// which is different from unicode.IsSpace, which also applies line feed as a space.
+func isSpace(r rune) bool {
+	switch r {
+	case '\t', '\v', '\f', '\r', ' ', 0x85, 0xA0:
+		return true
+	}
+	return false
+}
+
+func isLineEnd(r rune) bool {
+	if r == '\n' || r == '\r' {
+		return true
+	}
+	return false
+}
