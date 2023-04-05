5214c7a6486a0f2eb3d1c6394c139d02f5c7f1dc
pchchv <ipchchv@gmail.com>
2023-04-05 14:26:48 +0300

    Implement an UnmarshalBytes func

--- env.go
+++ env.go
@@ -1,5 +1,14 @@
 package env
 
+// UnmarshalBytes parses env file from byte slices of characters,
+// returning a map of keys and values.
+func UnmarshalBytes(src []byte) (map[string]string, error) {
+	out := make(map[string]string)
+	err := parseBytes(src, out)
+
+	return out, err
+}
+
 func filenamesOrDefault(filenames []string) []string {
 	if len(filenames) == 0 {
 		return []string{".env"}
