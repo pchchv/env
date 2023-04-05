1d2f2196763546d9edd927582e768d61653f1382
pchchv <ipchchv@gmail.com>
2023-04-05 11:09:23 +0300

    Implement a getStatementStart func

--- parser.go
+++ parser.go
@@ -5,6 +5,8 @@ import (
 	"unicode"
 )
 
+const charComment = '#'
+
 func indexOfNonSpaceChar(src []byte) int {
 	return bytes.IndexFunc(src, func(r rune) bool {
 		return !unicode.IsSpace(r)
@@ -15,4 +17,26 @@ func isChar(char rune) func(rune) bool {
 	return func(v rune) bool {
 		return v == char
 	}
+}
+
+// getStatementPosition returns the start position of the statement.
+// It skips any comment string or character that is not a space.
+func getStatementStart(src []byte) []byte {
+	pos := indexOfNonSpaceChar(src)
+	if pos == -1 {
+		return nil
+	}
+
+	src = src[pos:]
+	if src[0] != charComment {
+		return src
+	}
+
+	// skip comment section
+	pos = bytes.IndexFunc(src, isChar('\n'))
+	if pos == -1 {
+		return nil
+	}
+
+	return getStatementStart(src[pos:])
 }
